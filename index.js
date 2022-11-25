9148e73b77d7fb188a435b1d3963be9b2730f841
INFINITY <61787989+Niraj-Dilshan@users.noreply.github.com>
2022-11-25 20:31:48 +0000

    deploy-slash-commands

--- index.js
+++ index.js
@@ -1,28 +1,72 @@
-const Client = require("discord.js");
-const { MessageEmbed } = require('discord.js')
-const { readdirSync } = require('fs');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const fs = require('node:fs');
+const path = require('node:path');
+const { Collection } = require('discord.js');
 const axios = require('axios');
 const chatbot_name = "Nezuko";
 const chatbot_gender = "Female";
-const client = new Client({disableEveryone: true,});
 require('dotenv').config()
+
 const Token = process.env.TOKEN;
-client.login(Token);
+const ChanelID = process.env.CHANNEL_ID;
 
-client.on("message", async message =>{
-   if (message.author.bot) return;  //Check Message Was Send By A Bot
-   if (message.channel.id === "ChanelID"|message.channel.id === "ChanelID")   //Check Only Messages On This Chanels
-    {
-      if(!message) throw new Error("Error. No message provided")  //Check Message Not Emty
-      const res = await axios.get(`https://api.udit.gq/api/chatbot?message=${message}&gender=${chatbot_gender}&name=${chatbot_name}`)
-      .then((response) => {
-      const msg = response.data.message;
-      message.channel.send(`${msg}`);
-      }, (error) => {
-        console.log(error);
-      });
-    }else{
-      return;
-   }
- });
 
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(`Error executing ${interaction.commandName}`);
+		console.error(error);
+	}
+});
+
+ client.login(Token);
